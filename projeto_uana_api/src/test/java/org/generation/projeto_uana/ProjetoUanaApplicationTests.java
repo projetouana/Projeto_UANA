7818e49a07b73c4f8bfd0b50fc37933b3e262387
unknown <you@example.com>
2022-01-21 11:04:29 -0300

    Task 3/4

---
+++ projeto_uana_api/src/test/java/org/generation/projeto_uana/ProjetoUanaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.projeto_uana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoUanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
